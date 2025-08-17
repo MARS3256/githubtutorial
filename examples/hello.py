be3544dc70950b402b26111a459c218bbc89491b
MARS <mars.vxi3@gmail.com>
2025-08-17 06:02:26 +0600

    Update examples with improvements

--- examples/hello.py
+++ examples/hello.py
@@ -1,7 +1,10 @@
 # simple hello world
 
+# Edit this DEFAULT_GREETING on different remotes/branches to practice forks and pull requests
+DEFAULT_GREETING = "Hello, world!"  # <-- change this line in exercises
+
 def main():
-    print("Hello, world!")
+    print(DEFAULT_GREETING)
 
 if __name__ == '__main__':
     main()
--- examples/stars.py
+++ examples/stars.py
@@ -1,6 +1,6 @@
 # star pyramid example
 
-def pyramid(rows=5):
+def pyramid(rows=7):
     for i in range(1, rows+1):
         print(' '*(rows-i) + '*'*(2*i-1))
 
